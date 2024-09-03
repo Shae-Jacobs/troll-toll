d2807565ac2bf06064dbddfbabf1a58cdb9a2596
jem-halpine <29801095+jem-halpine@users.noreply.github.com>
2024-09-03 16:01:08 +1200

    more DB functions

--- server/db/favourite.ts
+++ server/db/favourite.ts
@@ -1,7 +1,16 @@
 import db from './connection.ts'
 import { Favourite, FavouriteData } from '../../models/favourite'
 
-export async function getFavourites(): Promise<Favourite[]> { 
+export async function getFavourites(): Promise<Favourite[]> {
   return db('favourites').select('*')
 }
 
+export async function getFavouriteById(id: number): Promise<Favourite> {
+  return db('favourites').where({ id }).select('*').first()
+}
+
+export async function deleteFavouriteById(id: number) {
+  return db('favourites').where({ id }).delete().first()
+}
+
+export async function addFavourite(favourite: Favourite) {}
