c0b69d9a26f0259a6ab81c9ecb048a97b45ee2c5
Lernaen <171352496+Lernaen@users.noreply.github.com>
2024-09-04 11:14:29 +1200

    found end route for brdiges complete

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,4 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import Layout from './components/Layout.tsx'
+import Bridges from './components/Bridges.tsx'
 
-export default createRoutesFromElements(<Route index element={<Layout />} />)
+export default createRoutesFromElements(
+  <Route element={<Layout />}>
+    <Route index element={<Bridges />} />
+  </Route>,
+)
