60309a003450859c3d805d4ae3ea84c135fbeb10
jem-halpine <29801095+jem-halpine@users.noreply.github.com>
2024-09-03 17:24:17 +1200

    add usertokens to db fn's

--- server/db/favourite.ts
+++ server/db/favourite.ts
@@ -1,21 +1,30 @@
 import db from './connection.ts'
 import { Favourite } from '../../models/favourite'
-import { Bridge } from '../../models/bridge.ts'
 
-export async function getFavourites(): Promise<Favourite[]> {
-  return db('favourites').select('*')
+export async function getFavourites(userToken: string): Promise<Favourite[]> {
+  return db('favourites').where('users_token', userToken).select('*')
 }
 
-export async function getFavouriteById(id: number): Promise<Favourite> {
-  return db('favourites').where({ id }).select('*').first()
-}
-
-export async function deleteFavouriteById(id: number) {
-  return db('favourites').where({ id }).delete().first()
-}
-
-export async function addFavourite(favourite: Favourite) {
+export async function getFavouriteById(
+  id: number,
+  userToken: string,
+): Promise<Favourite> {
   return db('favourites')
-    .insert(favourite)
-    .then((insertedEntries) => insertedEntries[0])
+    .where({ id })
+    .where('users_token', userToken)
+    .select('*')
+    .first()
+}
+
+export async function deleteFavouriteById(id: number, userToken: string) {
+  return db('favourites')
+    .where({ id })
+    .where('users_token', userToken)
+    .delete()
+    .first()
+}
+
+export async function addFavourite(newFave: Favourite, userToken: string) {
+  return db('favourites').where('users_token', userToken)
+  // .then((insertedEntries) => insertedEntries[0])
 }
