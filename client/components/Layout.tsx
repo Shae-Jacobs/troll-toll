11dd4f7097a89608acb6942b09eab74626e84085
Hemi Phillips <hello.hemi.phillips@gmail.com>
2024-09-04 21:25:04 +1200

    Add simple page structure styles

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 
 export default function Layout() {
   return (
-    <>
+    <div className="page grid min-h-[100svh] grid-rows-[auto_1fr_auto]">
       <header>
         <Navbar />
       </header>
@@ -11,6 +11,6 @@ export default function Layout() {
         <Outlet />
       </main>
       <footer></footer>
-    </>
+    </div>
   )
 }
