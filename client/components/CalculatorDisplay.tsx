d75b130130162c95d17246060a2226aed3ede511
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-04 13:36:45 +1200

    gettingCandies total sum, CalculatorDisplay component

--- client/components/CalculatorDisplay.tsx
+++ client/components/CalculatorDisplay.tsx
@@ -8,6 +8,10 @@ interface Props {
   id: number
 }
 
+//1Ȼ is 1 troll rock candy, the smallest division of currency
+// 100 Rock Candies = 1 Gold Ring (AuR) : 10Ȼ = 1AuR
+// 100 Gold Rings = 1 Goat (GT) : 100AuR = 1 GT
+
 function CalculatorDisplay({ user, id }: Props) {
   const { data, isPending, isError, error } = useToll(user, id)
 
@@ -19,7 +23,26 @@ function CalculatorDisplay({ user, id }: Props) {
     return <p>Failed {String(error)}</p>
   }
 
-  return <p>Calculate Toll!</p>
+  //map of candies object and extract just the candies
+
+  function gettingCandies() {
+    const result = data?.map((candies) => candies.candies)
+
+    const reduced = result?.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue
+    }, 0)
+
+    return reduced
+  }
+
+  console.log(gettingCandies())
+
+  return (
+    <>
+      <p>Calculate Toll!</p>
+      <p>Total Candies: {gettingCandies()}</p>
+    </>
+  )
 }
 
 export default CalculatorDisplay
