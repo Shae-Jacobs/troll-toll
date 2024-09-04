3e9450c1ba1a3e881ffe47526a3463c84412c2ea
Shae <171490998+Shae-Jacobs@users.noreply.github.com>
2024-09-04 16:57:27 +1200

    Layout nav bar grid

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -3,10 +3,10 @@ import Logo from './Logo'
 import Login from './Login'
 export default function Navbar() {
   return (
-    <>
+    <div className="navbar grid grid-cols-[10rem_1fr_auto] place-items-center ">
       <Logo />
       <RedirectList />
       <Login />
-    </>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './client/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'accent-1': '#CEEAFF',
+        'accent-2': '#9DCCF1',
+        'accent-3': '#69B1E9',
+        'accent-4': '#1D7ECA',
+        'accent-5': '#03045E',
+        'accent-bg': '#F3FBFF',
+      },
+    },
   },
   plugins: [],
 }
