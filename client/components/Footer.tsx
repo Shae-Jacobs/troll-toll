c2ef73ddff5c433536215133d262bf92b30c616f
fran-berridge <171234083+fran-berridge@users.noreply.github.com>
2024-09-05 16:41:06 +1200

    created footer

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom'
-
 export default function Footer() {
   return (
     <div className="footer grid min-h-24 grid-cols-[auto_1fr_auto] items-center gap-16 bg-accent-bg px-16 py-4 font-bold">
