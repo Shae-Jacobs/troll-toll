77cb50bef10285124a8028e2cf9eb1955cbe7ba3
Rowan Strang <rowanstrang@gmail.com>
2024-09-06 11:10:35 +1200

    CSS finished

--- client/components/SetToll.tsx
+++ client/components/SetToll.tsx
@@ -16,14 +16,16 @@ function SetToll({ id }: Props) {
   return (
     <>
       <form
-        className="setValueContainermd m-4 mx-auto grid min-h-60 w-96 justify-center rounded-lg bg-accent-2 p-4 px-4"
+        className="setValueContainer md m-4 mx-auto grid min-h-60 w-[28rem] justify-center rounded-lg bg-accent-2 p-4 px-4"
         onSubmit={(evt) => evt.preventDefault()}
         aria-label="Toll Value"
       >
-        <div>
-          <label htmlFor="name">Set Your Toll Value: </label>
+        <div className="flex flex-col items-center space-y-3">
+          <label htmlFor="name" className="text-2xl font-bold">
+            Set Your Toll Value:{' '}
+          </label>
           <input
-            className="form"
+            className="w-full max-w-xs rounded-md border border-gray-300 px-4 py-2 text-center shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
             type="number"
             name="toll"
             id="toll"
