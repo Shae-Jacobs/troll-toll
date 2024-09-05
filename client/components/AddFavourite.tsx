fac4ef8b6a3ea7ecdcfdf2aedafbcc67c1134732
jem-halpine <29801095+jem-halpine@users.noreply.github.com>
2024-09-05 12:19:26 +1200

    first try at AddFavourite component

---
+++ client/components/AddFavourite.tsx
@@ -0,0 +1,25 @@
+import { useAddFavourites } from '../hooks/useFavourite'
+
+// interface Props {
+//   user: string
+//   bridgeId: number
+// }
+
+function AddFavourite() {
+  const addFave = useAddFavourites()
+  const handleClick = async () => {
+    try {
+      await addFave.mutate({
+        usersToken: '0auth|4321',
+        bridgesId: 3,
+      })
+    } catch (error) {
+      console.error('Error Adding Favourite')
+    }
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>Add to favourites</button>
+    </div>
+  )
+}
