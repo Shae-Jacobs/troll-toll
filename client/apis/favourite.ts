8c4055399ba6949d1584da668279f374fc1511dd
jem-halpine <29801095+jem-halpine@users.noreply.github.com>
2024-09-05 15:31:21 +1200

    Add to favourites working

--- client/apis/favourite.ts
+++ client/apis/favourite.ts
@@ -4,7 +4,7 @@ import { Favourite, FavouriteData } from '../../models/favourite'
 //GET 'api/v1/favourites'
 export async function getFavourites(usersToken: string): Promise<Favourite[]> {
   const result = await request
-    .get(`/api/v1/favourites/`)
+    .get(`/api/v1/favourites`)
     .set('Authorization', `Bearer ${usersToken}`)
 
   return result.body as Favourite[]
@@ -12,18 +12,20 @@ export async function getFavourites(usersToken: string): Promise<Favourite[]> {
 
 //GET 'api/v1/favourite/:user/:id'
 export async function getFavouriteById(user: string, id: number) {
-  const result = await request.get(`/api/v1/favourite/${user}/${id}`)
+  const result = await request.get(`/api/v1/favourites/${user}/${id}`)
   return result.body as FavouriteData
 }
 
 //DELETE 'api/v1/favourite/:user/:id'
 export async function deleteFavouriteById(user: string, id: number) {
-  const result = await request.delete(`/api/v1/favourite/${user}/${id}`)
+  const result = await request.delete(`/api/v1/favourites/${user}/${id}`)
   console.log(result.statusCode)
+  return
 }
 
 //POST 'api/v1/favourite
 export async function addFavourite(newFave: Favourite) {
-  const result = await request.post(`/api/v1/favourite`).send(newFave)
+  const result = await request.post(`/api/v1/favourites`).send(newFave)
   console.log(result.statusCode)
+  return
 }
