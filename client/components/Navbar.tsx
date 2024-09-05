64bc83f63d9f002ecd1d1885774565269043297c
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-06 11:33:12 +1200

    Nav added back in

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Logo from './Logo'
+import Nav from './Nav'
 import UserAuthArea from './UserAuthArea'
 
 export default function Navbar() {
   return (
     <div className="navbar grid min-h-24 grid-cols-[auto_1fr_auto] items-center gap-16 bg-accent-bg px-16 py-4">
       <Logo />
-      <div></div>
+      <Nav />
       <UserAuthArea />
     </div>
   )
